c16265bf8dcf2a8da8c33a1a29f5172366ba22f5
alex lin <lingang_upc@163.com>
2016-03-21 10:20:13 +0800

    init project

---
+++ house_crawlers/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class HouseCrawlersItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
