09351bd795e4209664fe77b0c3e7cec456beffb1
alex lin <lingang_upc@163.com>
2016-03-22 13:13:55 +0800

    mysqlpipeline completed

--- house_crawlers/pipelines/mysqldbpipeline.py
+++ house_crawlers/pipelines/mysqldbpipeline.py
@@ -5,17 +5,17 @@ Created on 2016-03-21
 
 @author: alex
 '''
-
+import logging
 from twisted.enterprise import adbapi
 
 class MySQLPipeline(object):
     '''
     数据写入 mysql的管道
     '''
-    def __init__(self, logger, stats, mysql_info):
+    def __init__(self, stats, mysql_info):
         self.dbpool = adbapi.ConnectionPool('MySQLdb', **mysql_info)
         self.stats = stats
-        self.logger = logger
+        self.logger = logging.getLogger(__name__)
         
     @classmethod
     def from_crawler(cls, crawler):
@@ -24,21 +24,23 @@ class MySQLPipeline(object):
         '''
         mysql_info = crawler.settings.get('MYSQL_INFO')
         stats = crawler.stats
-        logger = crawler.logger
-        return cls(logger, stats, mysql_info)
+        return cls(stats, mysql_info)
         
     def process_item(self, item, spider):
-        query = self.dbpool.runInteraction(self.__insert, item)
+        query = self.dbpool.runInteraction(self._insert, item)
         query.addErrback(self._handle_error)
         return item
     
     def _insert(self, tx, item):
-        result = tx.execute(""" 
-        insert into table house_info(house_name,house_index_url,house_price,house_address,longtitude_latitude)
+        tx.execute(""" 
+        insert into  house_info(house_name,house_index_url,house_price,house_address,longtitude_latitude)
         values (%s,%s,%s,%s,%s)""",(item['house_name'],item['house_index_url'],item['house_price'],item['house_address'],item['longtitude_latitude']))
         
-        if result > 0:
-            self.stats.inc_value('mysql_items_added', count=1, start=0)
-        
+        self.stats.inc_value('mysql_items_added', count=1, start=0)
+             
     def _handle_error(self,e):
+        self.stats.inc_value('mysql_items_failed', count=1, start=0)
         self.logger.error(e)
+    
+    def close_spider(self,spider):
+        self.dbpool.close()
--- house_crawlers/spiders/sinahousespider.py
+++ house_crawlers/spiders/sinahousespider.py
@@ -8,23 +8,23 @@ Created on 2016-03-21
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from items.sinahouseitem import SinahouseItem
+from house_crawlers.items.sinahouseitem import SinahouseItem
 
 class SinahouseSpider(CrawlSpider):
     """
     新浪乐居楼盘爬虫
     """
     name = 'sinahouse'
-    allowed_domains = ['house.sina.com',]
+    allowed_domains = ['house.sina.com.cn',]
     start_urls = ['http://data.house.sina.com.cn/sh/search/?bcity=sh&keyword=',]
     costom_settings = {}
     rules = [
              #具体楼盘链接提取
              Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+\d+\?\w+=\w+bt\d*")), callback='parse_item'),
              #当前城市下,楼盘下一页链接跟进
-             Rule(LinkExtractor(allow=("/\w+/search-\d+.*")), follow=True),
-             #其他城市链接跟进
-             Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
+#              Rule(LinkExtractor(allow=("/\w+/search-\d+.*")), follow=True),
+#              #其他城市链接跟进
+#              Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
              ]
     
     def parse_item(self,response):
@@ -34,8 +34,8 @@ class SinahouseSpider(CrawlSpider):
         item = SinahouseItem()
         item['house_name'] = response.xpath('/html/body/div[1]/div[9]/div[1]/h2/text()').extract_first().strip()
         item['house_index_url'] = response.url
-        item['house_price'] = response.xpath('//*[@id="callmeBtn"]/ul/li[1]/em[1]').extract_first().strip()
-        item['house_address'] = response.xpath('//*[@id="callmeBtn"]/ul/li[2]/span[2]').extract_first().strip()
-        item['longtitude_latitude'] = [float(response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'")),float(response.xpath('//script').re_first(".*?coordy='(\d+.\d+)'.*"))]
+        item['house_price'] = ' '.join(response.xpath('//*[@id="callmeBtn"]/ul/li[1]/em[1]/text() | //*[@id="callmeBtn"]/ul/li[1]/em[2]/text()').extract())
+        item['house_address'] = response.xpath('//*[@id="callmeBtn"]/ul/li[2]/span[2]/text()').extract_first().strip()
+        item['longtitude_latitude'] = ','.join([response.xpath('//script').re_first(".*?coordx='(\d+.\d+)'"), response.xpath('//script').re_first(".*?coordy='(\d+.\d+)'.*")])
         
         return item
