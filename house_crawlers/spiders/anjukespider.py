a41ac3f204263785941ec3e45877cdd2abe743a9
alex lin <lingang_upc@163.com>
2016-03-24 11:09:14 +0800

    anjuke complete

--- house_crawlers/spiders/anjukespider.py
+++ house_crawlers/spiders/anjukespider.py
@@ -7,6 +7,7 @@ Created on 2016-03-22
 '''
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from house_crawlers.items.anjukeitem import AnjukeItem
 
 class AnjukeSpider(CrawlSpider):
     """
@@ -21,6 +22,18 @@ class AnjukeSpider(CrawlSpider):
              #城市新楼盘页面跟进
              Rule(LinkExtractor(allow=('^http://\w+.fang.anjuke.com$')),follow=True),
              #城市新楼盘翻页跟进
-             Rule(LinkExtractor(allow=('^http://\w+.fang.anjuke.com/\w+/s\?p=\d+$')),follow=True),
-             
+             Rule(LinkExtractor(allow=('^http://\w+.fang.anjuke.com/loupan/s\?p=\d+$')),follow=True),
+             #楼盘详情页
+             Rule(LinkExtractor(allow=('^http://\w+.fang.anjuke.com/loupan/\d+.html$')),callback="parse_item"),
              ]
+    def parse_item(self, response):
+        item = AnjukeItem()
+        item['house_name'] = response.xpath('//*[@id="j-triggerlayer"]/text()').extract_first().strip()
+        item['house_index_url'] = response.url
+        item['house_price'] = response.xpath('//*[@id="container"]/div[1]/div[2]/div[1]/dl/dd[1]/p/text()[1]').extract_first(default='').strip()+\
+            response.xpath('//*[@id="container"]/div[1]/div[2]/div[1]/dl/dd[1]/p/em/text()').extract_first(default='').strip() +\
+            response.xpath('//*[@id="container"]/div[1]/div[2]/div[1]/dl/dd[1]/p/text()[2]').extract_first(default='').strip()
+        item['house_address'] = response.xpath('//*[@id="container"]/div[1]/div[2]/div[1]/dl/dd[4]/span/text()').extract_first()
+        item['longtitude_latitude'] = response.xpath('//script').re_first('.*?lng: (\d+.\d+)') + ',' + response.xpath('//script').re_first('.*?lat: (\d+.\d+)')
+        return item
+        
