d49b63e60ede410f4b129ba0fe2a5daacaf7911c
alex lin <lingang_upc@163.com>
2016-03-24 12:02:50 +0800

    update sinahouse xpath

--- house_crawlers/spiders/sinahousespider.py
+++ house_crawlers/spiders/sinahousespider.py
@@ -20,7 +20,7 @@ class SinahouseSpider(CrawlSpider):
     costom_settings = {}
     rules = [
              #其他城市链接跟进
-#             Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
+            Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
              #当前城市下,楼盘下一页链接跟进
             Rule(LinkExtractor(allow=("/\w+/search-\d+.*")), follow=True),
              #具体楼盘链接提取
@@ -32,7 +32,7 @@ class SinahouseSpider(CrawlSpider):
         提取具体楼盘信息
         """
         item = SinahouseItem()
-        item['house_name'] = response.xpath('/html/body/div[1]/div[9]/div[1]/h2/text()').extract_first().strip()
+        item['house_name'] = response.xpath('//div[@class="header04Left"]/h2/text()').extract_first().strip()
         item['house_index_url'] = response.url
         item['house_price'] = ' '.join(response.xpath('//*[@id="callmeBtn"]/ul/li[1]/em[1]/text() | //*[@id="callmeBtn"]/ul/li[1]/em[2]/text()').extract())
         item['house_address'] = response.xpath('//*[@id="callmeBtn"]/ul/li[2]/span[2]/text()').extract_first().strip()
