9cd83f2825dc161f8e20d6f6ee4526f256b66e5e
alex lin <lingang_upc@163.com>
2016-04-05 15:30:39 +0800

    test merge

--- house_crawlers/spiders/sinahousespider.py
+++ house_crawlers/spiders/sinahousespider.py
@@ -20,7 +20,7 @@ class SinahouseSpider(CrawlSpider):
     costom_settings = {}
     rules = [
              #其他城市链接跟进
-            Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
+            #Rule(LinkExtractor(allow=("http://data.house.sina.com.cn/\w+/search/.*")), follow=True),
              #当前城市下,楼盘下一页链接跟进
             Rule(LinkExtractor(allow=("/\w+/search-\d+.*")), follow=True),
              #具体楼盘链接提取
